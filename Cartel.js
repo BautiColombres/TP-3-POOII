c5789b4e32a16c9c23bf2befcf2920017e6113ea
BautiColombres <114516945+BautiColombres@users.noreply.github.com>
2022-10-22 14:34:58 -0300

    TDD iteration

--- Cartel.js
+++ Cartel.js
@@ -2,6 +2,9 @@ function Cartel(){
     this.matriz=[];
     for(var i=0;i<100;i++){
         this.matriz[i]=[];
+        for(var j=0;j<100;j++){
+            this.matriz[i][j]=0;
+        }
     }
 }
 
