d2ce6f3495785b9526092bce3f69467ca7fee0bc
BautiColombres <114516945+BautiColombres@users.noreply.github.com>
2022-10-22 15:24:09 -0300

    TDD iteration

--- tests/testCartel.test.js
+++ tests/testCartel.test.js
@@ -26,11 +26,12 @@ test("cuantas luces hay encendidas",()=>{
     expect(cartel.cuantasEncendidas()).toBe(0);
 });
 test("encender posiciones deseadas",()=>{
-    cartel.encenderLuces([2,1],[5,8]);
-    var aux=false;
-    if(cartel.cuantasEncendidas()>0){
-        aux=true;
-    }
-    expect(aux).toBe(true);
+    cartel.encenderLuces([0,0],[2,2]);
+    expect(cartel.cuantasEncendidas()).toBe(9);
     expect(()=>{cartel.encenderLuces([5,8],[2,1])}).toThrow(new Error("La posicion inicial es mayor que la final"));
+});
+test("apagar posiciones deseadas",()=>{
+    cartel.encenderLuces([2,1],[5,8]);
+    cartel.apagarLuces([2,1],[5,8]);
+    expect(cartel.cuantasEncendidas()).toBe(0);
 });
