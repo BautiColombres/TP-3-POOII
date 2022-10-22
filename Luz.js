6bbbb138c7e4866d11fc1b1160ef569b9b5c1ca2
BautiColombres <114516945+BautiColombres@users.noreply.github.com>
2022-10-22 13:25:45 -0300

    TDD iteration

--- Luz.js
+++ Luz.js
@@ -1,10 +1,17 @@
 function Luz(){
-    
+    this.estado=0;
     this.estadoActual=function(){
-        return "apagada";
+        if(this.estado==0){
+            return "apagada";
+        }
+        else if(this.estado==1){
+            return "encendida";
+        }
     }
     this.encender=function(){
-        
+        if (this.estado==0){
+            this.estado=1;
+        }
     }
 }
 
