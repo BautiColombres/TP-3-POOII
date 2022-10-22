7f4f2791655a367ed38fb85d251d0bad3133ffa6
BautiColombres <114516945+BautiColombres@users.noreply.github.com>
2022-10-22 15:28:27 -0300

    TDD iteration

--- Cartel.js
+++ Cartel.js
@@ -30,6 +30,9 @@ function Cartel(){
         }
     }
     this.apagarLuces=function(inicial,final){
+        if(inicial[0]>final[0] || inicial[1]>final[1]){
+            throw new Error("La posicion inicial es mayor que la final");
+        }
         for(var i=inicial[0];i<=final[0];i++){
             for(var j=inicial[1];j<=final[1];j++){
               this.matriz[i][j].apagar();
