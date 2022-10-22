61014204205e4eb57c65612535764941adfb9690
BautiColombres <114516945+BautiColombres@users.noreply.github.com>
2022-10-22 16:27:54 -0300

    TDD iteration

--- Luz.js
+++ Luz.js
@@ -4,6 +4,9 @@ function Luz(){
         return this.estado;
     }
     this.encender=function(){
+        if(this.estado==10){
+            throw new Error("La intensidad no puede ser mayor que 10");
+        }
         this.estado++;
     }
     this.apagar=function(){
