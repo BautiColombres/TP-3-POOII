9573529cdd1033028e9ef3d568be11247e703724
BautiColombres <114516945+BautiColombres@users.noreply.github.com>
2022-10-22 13:21:56 -0300

    TDD iteration

--- tests/testLuz.test.js
+++ tests/testLuz.test.js
@@ -4,3 +4,8 @@ test("crear luz apagada",()=>{
     const luz=new Luz();
     expect(luz.estadoActual()).toBe("apagada");
 });
+test("encender luz",()=>{
+    const luz=new Luz();
+    luz.encender();
+    expect(luz.estadoActual()).toBe("encendida");
+});
