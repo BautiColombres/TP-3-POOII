ebb87a546c0567b1ea00abc71d9be4a004fc4079
BautiColombres <114516945+BautiColombres@users.noreply.github.com>
2022-10-22 14:32:51 -0300

    TDD iteration

--- tests/testCartel.test.js
+++ tests/testCartel.test.js
@@ -2,12 +2,19 @@ const Cartel= require('./../Cartel');
 
 test("crear cartel",()=>{
     const cartel=new Cartel();
-    var cont=0;
+    var contadorColumnas=0;
+    var contadorLuces=0;
     for(var fila=0;fila<100;fila++){
         if(cartel.matriz[fila].length==100){
-            cont++;
+            contadorColumnas++;
+        }
+        for(var columna=0;columna<100;columna++){
+            if(cartel.matriz[fila][columna] instanceof Luz){
+                contadorLuces++;
+            }
         }
     }
     expect(cartel.matriz.length).toBe(100);
-    expect(cont).toBe(100);
+    expect(contadorColumnas).toBe(100);
+    expect(contadorLuces).toBe(10000);
 });
