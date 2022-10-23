71d8c8e8f6551e62cc9c7ad3e0736e352b781783
BautiColombres <114516945+BautiColombres@users.noreply.github.com>
2022-10-23 19:54:03 -0300

    TDD iteration

--- Cartel.js
+++ Cartel.js
@@ -46,9 +46,7 @@ function Cartel(){
         }
     }
     this.cambiarLuces=function(inicial,final){
-        if(inicial[0]>99 || inicial[0]<0 || inicial[1]>99 || inicial[1]<0 || final[0]>99 || final[0]<0 || final[1]>99 || final[1]<0){
-            throw new Error("Parametros erroneos");
-        }
+        chequearParametros(inicial,final);
         if(inicial[0]>final[0] || inicial[1]>final[1]){
             throw new Error("La posicion inicial es mayor que la final");
         }
@@ -61,6 +59,11 @@ function Cartel(){
                     this.matriz[i][j].apagar();
                 }
             }
+        }
+    }
+    var chequearParametros=function(inicial,final){
+        if(inicial[0]>99 || inicial[0]<0 || inicial[1]>99 || inicial[1]<0 || final[0]>99 || final[0]<0 || final[1]>99 || final[1]<0){
+            throw new Error("Parametros erroneos");
         }
     }
 }
